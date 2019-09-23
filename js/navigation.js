d6ad421d54e3273b9fa4a80118763ee8929a289f
Travis-Wolfe <51418822+Travis-Wolfe@users.noreply.github.com>
2019-09-23 12:33:48 -0600

    Bracket file upload

---
+++ js/navigation.js
@@ -0,0 +1,10 @@
+function openNav() {
+                            
+                            document.getElementById("sidebarNav").style.width = "250px";
+                            
+                        }
+                        function closeNav() {
+                            
+                            document.getElementById("sidebarNav").style.width = "0px";
+                    
+                        }
