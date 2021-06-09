791c5201f4f453f6958ee4dece4617fe5a5cbccf
Travis-Wolfe <51418822+Travis-Wolfe@users.noreply.github.com>
2021-06-09 11:43:42 -0600

    Mobile product pages implemented

--- js/mobile_nav.js
+++ js/mobile_nav.js
@@ -1,16 +1,17 @@
 var navigation = document.getElementById("nav-bar");
-var element = document.getElementById("burger");
+var menu = document.getElementById("burger");
+var products = document.getElementById("product-list");
 var leftX = document.getElementById("top");
 var middle = document.getElementById("middle");
 var rightX = document.getElementById("bottom");
 
 function toggleMenu() {
-    element.classList.toggle("collapse");
+    menu.classList.toggle("collapse");
     leftX.classList.toggle("rotate30");
     middle.classList.toggle("rotate30");
     rightX.classList.toggle("rotate30");
 
-    if(element.classList.contains("collapse")) {
+    if(menu.classList.contains("collapse")) {
         navigation.style.visibility = "visible";
         navigation.style.width = "200px";
     }
@@ -19,4 +20,12 @@ function toggleMenu() {
         navigation.style.visibility = "hidden";
         navigation.style.width = "0px";
     }
+}
+
+function toggleProducts() {
+
+    
+    products.classList.toggle("invisible");
+    products.classList.toggle("product-list");
+
 }
